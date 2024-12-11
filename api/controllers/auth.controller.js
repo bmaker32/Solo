dcba435fff3ca2bf58e7c8ee24293634790b3b11
bmaker32 <[bmekonnen422@gmail.com]>
2024-12-11 13:11:21 -0800

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
+export const SignUp = async(req,res) => {
+    const {username,email,password} = req.body;
+    const hashedPassword = bcrypt.hashSync(password,10);
+    const newUser = new User({
+        username,
+        email,
+        password:hashedPassword
+    });
+    try{
+        await newUser.save()
+        res.status(201).json({message:'User created successfully'})
+    } catch(error){
+        res.status(500).json({message:'User creation failed'})
+    }
+    
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -12,8 +13,12 @@ mongoose.connect(process.env.mongo).then(() => {
     console.log(err)
 });
 const app = express();
+app.use(express.json());
+
+
 app.listen(3003, () => {
     console.log('Server is running on http://localhost:3003 ');
     });
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
