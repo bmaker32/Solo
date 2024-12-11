e17961100a4bac7927dfbbe70cd8b6b17c7c64e0
bmaker32 <[bmekonnen422@gmail.com]>
2024-12-11 12:27:41 -0800

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ mongoose.connect(process.env.mongo).then(() => {
     console.log(err)
 });
 const app = express();
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000 ');
+app.listen(3003, () => {
+    console.log('Server is running on http://localhost:3003 ');
     });
+
+app.use('/api/user',userRoutes);
