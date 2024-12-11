70f6dfeb69a2fba57b08bc70039d01335bf9fc09
bmaker32 <[bmekonnen422@gmail.com]>
2024-12-11 11:41:29 -0800

    create and run backend

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+
+mongoose.connect(process.env.mongo).then(() => {
+    console.log('connected to database')
+}).catch((err) => {
+    console.log(err)
+});
 const app = express();
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000 ');
